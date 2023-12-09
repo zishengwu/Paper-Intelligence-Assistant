2b2b8396937a20d7633d6a33f174e14e0ede387c
深度学习机器 <wuzsgdutee@163.com>
2023-12-09 21:09:09 +0800

    Update and rename assistant.py to assistant_cli.py

--- src/assistant_cli.py
+++ src/assistant_cli.py
@@ -1,5 +1,5 @@
 from modelscope import AutoTokenizer, AutoModel, snapshot_download
-from paperextracter import extract_paper
+from paperextracter import extract_paper,extract_content
 import torch
 
 if __name__ == '__main__':
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     model_dir = snapshot_download("ZhipuAI/chatglm3-6b-32k", revision = "v1.0.0")
     tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
     model = AutoModel.from_pretrained(model_dir,trust_remote_code=True).quantize(8).cuda().eval()
-     
+    
     history = []
     file_content = None
     
@@ -19,12 +19,13 @@ if __name__ == '__main__':
         "3.按`q`退出\n" \
         "**************************\n"
         )
-        
+
         if choice == 'q':
             break
         elif choice.split('.')[-1] == 'pdf':
             try:
-                file_content = extract_paper(f"{file_path}"+choice)
+                file_content = extract_content(f"{file_path}"+choice)
+                assert(type(file_content))==str, "论文格式出错"
             except Exception as e:
                 print('论文获取失败:', e)
             else:
@@ -35,10 +36,17 @@ if __name__ == '__main__':
                 continue
             if len(history) == 0:
                 query = f"已知内容：{file_content}\n{choice}"
-            else:   
+            else:
                 query = f"{choice}"
-                
-            with torch.no_grad():
-                response, history = model.chat(tokenizer, query, history=history)
-            print(response)
+            
+            past_key_values = None
+            cur_len = 0
+            for response, history, past_key_values in model.stream_chat(tokenizer, 
+                                                                        query, 
+                                                                        history=history,
+                                                                        temperature=1,
+                                                                        past_key_values=past_key_values,
+                                                                        return_past_key_values=True):
+                print(response[cur_len:], end="", flush=True)
+                cur_len = len(response)
             
